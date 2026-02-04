cc2f4aeb196fefa9a1e3f1d3f4c54a48bb24c802
MD_AALAM <mdmajid9817728180@gmail.com>
2026-02-04 20:04:21 +0300

    max_Depth_of_binary_tree.java

---
+++ Tree_problem/max_Depth_of_binary_tree.java
@@ -0,0 +1,34 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
+public class max_Depth_of_binary_tree {
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        int result = maxDepth(root);
+
+        System.out.println("Maximum Depth of Binary Tree:");
+        System.out.println(result);
+    }
+}
