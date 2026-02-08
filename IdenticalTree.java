4768693335c2a5171198e4a3c7b3953bb4159d20
MD_AALAM <mdmajid9817728180@gmail.com>
2026-02-09 00:19:12 +0300

    add IdenticalTree.java file

---
+++ IdenticalTree.java
@@ -0,0 +1,43 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+
+public class IdenticalTree {
+    public static void main(String[] args) {
+
+        // Tree 1
+        TreeNode p = new TreeNode(1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(3);
+
+        // Tree 2
+        TreeNode q = new TreeNode(1);
+        q.left = new TreeNode(2);
+        q.right = new TreeNode(3);
+
+        Solution sol = new Solution();
+        boolean result = sol.isSameTree(p, q);
+
+        System.out.println("Are both trees same? " + result);
+    }
+}
