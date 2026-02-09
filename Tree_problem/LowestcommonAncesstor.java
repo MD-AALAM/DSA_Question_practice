c4352e0bfa33e5fc736a8e5eae2e94ddd45c7723
MD_AALAM <mdmajid9817728180@gmail.com>
2026-02-09 23:36:12 +0300

    Add LowestcommonAncesstor.java

---
+++ Tree_problem/LowestcommonAncesstor.java
@@ -0,0 +1,63 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
+
+public class LowestCommonAncestor {
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left == null) {
+            return right;
+        }
+        if (right == null) {
+            return left;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(1);
+
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+
+        root.left.right.left = new TreeNode(7);
+        root.left.right.right = new TreeNode(4);
+
+        TreeNode p = root.left;             
+        TreeNode q = root.left.right.right;  
+
+        TreeNode lca = lowestCommonAncestor(root, p, q);
+
+        if (lca != null) {
+            System.out.println("Lowest Common Ancestor: " + lca.val);
+        } else {
+            System.out.println("LCA not found");
+        }
+    }
+}
