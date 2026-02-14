c5d17f7cdc2c67ba9d50f3d9cc7b1eeb779ea0c4
MD_AALAM <mdmajid9817728180@gmail.com>
2026-02-14 17:36:09 +0300

    Add zigzag level order traversal implementation using BFS

---
+++ Tree_problem/LevelOrderZigzagTraversal.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+// Definition for Binary Tree Node
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Solution {
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        if (root == null) {
+            return ans;
+        }
+
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        boolean leftToRight = true;
+
+        while (!queue.isEmpty()) {
+
+            List<Integer> levelval = new ArrayList<>();
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode curNode = queue.removeFirst();
+                levelval.add(curNode.val);
+
+                if (curNode.left != null) {
+                    queue.add(curNode.left);
+                }
+
+                if (curNode.right != null) {
+                    queue.add(curNode.right);
+                }
+            }
+
+            if (!leftToRight) {
+                Collections.reverse(levelval);
+            }
+
+            ans.add(levelval);
+            leftToRight = !leftToRight;
+        }
+
+        return ans;
+    }
+}
+
+public class LevelOrderZigzagTraversal {
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+
+        Solution sol = new Solution();
+        List<List<Integer>> result = sol.zigzagLevelOrder(root);
+
+        System.out.println("Zigzag Level Order Traversal:");
+        for (List<Integer> level : result) {
+            System.out.println(level);
+        }
+    }
+}
