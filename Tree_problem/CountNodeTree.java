60533ca8324fe046fca0ef8ea13d7356fb6cd4fb
MD_AALAM <mdmajid9817728180@gmail.com>
2026-02-16 20:12:37 +0300

    Add recursive solution to count total nodes in a binary tree

---
+++ Tree_problem/CountNodeTree.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftNode = countNodes(root.left);
+        int rightNode = countNodes(root.right);
+
+        return leftNode + rightNode + 1;
+    }
+}
+
+public class CountNodeTree {
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        Solution sol = new Solution();
+        int totalNodes = sol.countNodes(root);
+
+        System.out.println("Total number of nodes: " + totalNodes);
+    }
+}
