f46b7c220d9afcfe8beaed3058e71691652f1909
MD_AALAM <mdmajid9817728180@gmail.com>
2026-02-18 20:03:43 +0300

    Add Faltu file in a Tree problem

---
+++ Tree_problem/Faltu.java
@@ -0,0 +1,52 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Solution {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == val) {
+            return root;
+        } 
+        else if (root.val < val) {
+            return searchBST(root.right, val);
+        } 
+        else {
+            return searchBST(root.left, val);
+        }
+    }
+}
+
+public class Faltu {
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+
+        Solution sol = new Solution();
+
+        int target = 2;
+        TreeNode result = sol.searchBST(root, target);
+
+        if (result != null) {
+            System.out.println("Value found: " + result.val);
+        } else {
+            System.out.println("Value not found");
+        }
+    }
+}
