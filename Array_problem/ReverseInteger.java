4ddb476e3a4a77728b626be13c619a751200ecfd
MD_AALAM <mdmajid9817728180@gmail.com>
2026-02-20 18:21:13 +0300

    Add ReverseInteger solution in Array on java

---
+++ Array_problem/ReverseInteger.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class Solution {
+
+    public int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+
+            // Overflow check
+            if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+
+            rev = rev * 10 + digit;
+            x /= 10;
+        }
+
+        return rev;
+    }
+}
+
+public class ReverseInteger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int x = sc.nextInt();
+
+        Solution sol = new Solution();
+        int result = sol.reverse(x);
+
+        System.out.println("Reversed number: " + result);
+
+        sc.close();
+    }
+}
