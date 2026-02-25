8af9b2c5bfbb56525fa8c679ecba5c7ba0000880
MD_AALAM <mdmajid9817728180@gmail.com>
2026-02-25 17:40:36 +0300

    Add SetMissmatch in array on java

---
+++ Array_problem/SetMissmatch.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class SetMissmatch {
+
+    public static int[] findErrorNums(int[] nums) {
+        int dup = -1, missing = -1;
+
+        for (int i = 1; i <= nums.length; i++) {
+            int count = 0;
+
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] == i) {
+                    count++;
+                }
+            }
+
+            if (count == 2) {
+                dup = i;
+            } 
+            else if (count == 0) {
+                missing = i;
+            }
+        }
+
+        return new int[] {dup, missing};
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 2, 4};
+
+        int[] result = findErrorNums(nums);
+
+        System.out.println("Duplicate number: " + result[0]);
+        System.out.println("Missing number: " + result[1]);
+    }
+}
